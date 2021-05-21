3e4878572a97024563023d10b239e04c6f5fd73b
Linnea Uden <Linnea.uden@skola.botkyrka.se>
2021-05-21 10:44:23 +0200

    Added home, about and project pages

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,9 +1,27 @@
 import React from "react";
+import Typical from 'react-typical'
+import '../styles/Home.css'
+import { Link } from "react-router-dom";
+
+
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <h1>Hello!</h1>
+        <h3>
+            <Typical 
+            loop={1}
+            wrapper="p"
+            steps={[
+                ' My name is Linnea',
+                3000,
+                ' I am a web developer💻',
+                3000
+            ]}
+            />
+        </h3>
+        <Link to="/about" className="read-btn">Read More</Link>
     </div>
   );
 };
--- app/src/pages/Projects.js
+++ app/src/pages/Projects.js
@@ -1,9 +1,28 @@
 import React from "react";
-
+import '../styles/Projects.css';
+import shop from '../images/shop.png';
+import smartTimer from '../images/smartTimer.png';
+import vklass from '../images/vklass.png';
 export const Projects = () => {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className="project">
+    <div className="flex-item-left">
+      <img src={vklass} alt="" srcset="" />
+      <h3>Vklass</h3>
+      <p>This is a website I made in school, were I remade a already existing website</p>
     </div>
+    <div className="flex-item-middle">
+      <img src={smartTimer} alt="" srcset="" />
+      <h3>Timer</h3>
+      <p>Smart timer is an web application that is meant to help you with your rubik's cube career.</p>
+
+    </div>
+    <div className="flex-item-right">
+      <img src={shop} alt="" srcset="" />
+      <h3>Shop</h3>
+      <p>This is an product shop made in HTML, CSS and Javascript</p>
+    </div>
+
+</div>
   );
 };
