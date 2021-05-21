3eb7f3034b2651fae8d85df8a0cde5cc66f519b0
Linnea Uden <Linnea.uden@skola.botkyrka.se>
2021-05-21 09:28:03 +0200

    Created a responsive navbar

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import './styles/App.css';
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import {About} from "./pages/About";
+import { Blog } from "./pages/Blog";
+import { Contact } from "./pages/Contact";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Router>
+        <NavBar />
 
-    </div>
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
